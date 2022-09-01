3fc64d13124a006ab8d1b6966bc0f7f885bacfcb
gembright stone hung <34207794+gemhung@users.noreply.github.com>
2022-09-01 18:51:29 +0900

    Fix: make it sound 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use thiserror::Error;
+use tokio::sync::mpsc;
 use tracing::*;
 
 type TransactionID = u32;
@@ -56,13 +57,13 @@ pub enum DisputeStatus {
 #[derive(Error, Debug)]
 pub enum PaymentError {
     #[error("Duplicate transaction id, {0:?}")]
-    DuplicateTX(Transaction),
+    DuplicateTransaction(Transaction),
 
     #[error("Insuffiecient balance, tx = {0:?}, availabe = {1}")]
     InsuffiecientBalance(Transaction, Amount),
 
     #[error("No such transaction id, txn = {0:?}")]
-    NoSuchTX(Transaction),
+    NoSuchTransactionID(Transaction),
 
     #[error("Invalid disputed status, txn = {0:?}, current status = {1:?}")]
     InvalidDisputeStatus(Transaction, DisputeStatus),
@@ -70,24 +71,26 @@ pub enum PaymentError {
     #[error("Account is locked after tx = {1}, txn={0:?}")]
     Locked(Transaction, TransactionID), //
 
+    #[error("No endpoint to send({0:?})")]
+    NoEndpointToSend(Transaction),
+
     #[error("Unknown error")]
     Unknown(Transaction, Option<TransactionInner>),
 }
 
 #[derive(Debug, Clone)]
-struct FinanceBook {
-    client: ClientID,
-    asset: Asset,
+struct PaymentSummary {
+    asset_book: AssetBook,
     ledger: Vec<Transaction>,
     //disputed_records: std::collections::HashSet<TransactionID>,
-    is_locked: bool,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct Asset {
     total: f64,
     available: f64,
     hold: f64,
+    is_locked: Option<TransactionID>,
 }
 
 impl Asset {
@@ -96,6 +99,7 @@ impl Asset {
             total: 0.0,
             available: 0.0,
             hold: 0.0,
+            is_locked: None,
         }
     }
 }
@@ -103,41 +107,74 @@ impl Asset {
 type ServiceEndpoint = tokio::sync::mpsc::UnboundedSender<Transaction>;
 
 struct PaymentEngine {
-    endpoints: std::collections::HashMap<ClientID, ServiceEndpoint>,
-    dead_letter_queue: (
-        tokio::sync::mpsc::UnboundedSender<PaymentError>,
-        tokio::sync::mpsc::UnboundedReceiver<PaymentError>,
-    ),
+    endpoint: Vec<ServiceEndpoint>,
     finance_queue: (
-        tokio::sync::mpsc::UnboundedSender<FinanceBook>,
-        tokio::sync::mpsc::UnboundedReceiver<FinanceBook>,
+        tokio::sync::mpsc::UnboundedSender<PaymentService>,
+        tokio::sync::mpsc::UnboundedReceiver<PaymentService>,
     ),
+    shutdown: Option<tokio::task::JoinHandle<()>>,
 }
 
 impl PaymentEngine {
     fn new() -> Self {
         Self {
-            endpoints: std::collections::HashMap::new(),
-            dead_letter_queue: tokio::sync::mpsc::unbounded_channel(),
+            endpoint: vec![],
             finance_queue: tokio::sync::mpsc::unbounded_channel(),
+            shutdown: None,
         }
     }
 
-    fn start() {}
-
-    fn execute(
+    // start the enginge with a number of services
+    fn start(
         &mut self,
-        txn: Transaction,
-    ) -> Result<(), tokio::sync::mpsc::error::SendError<Transaction>> {
-        match self.endpoints.get(&txn.client_id()) {
-            Some(ep) => {
-                ep.send(txn);
-            }
-
-            None => {}
+        size: std::num::NonZeroU8,
+        dead_letter_queue: mpsc::UnboundedSender<PaymentError>,
+    ) {
+        let sz = size.get() as usize;
+        let mut wait_group = vec![];
+        wait_group.reserve(sz);
+        for _ in 0..sz {
+            let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+            let dead_que = dead_letter_queue.clone();
+            // update wait group
+            wait_group.push(tokio::spawn(async move {
+                PaymentService::new().run(rx, dead_que).await
+            }));
+            // update endpoint
+            self.endpoint.push(tx);
         }
 
+        // when service ended, we get summary
+        self.shutdown = Some(tokio::spawn(async move {
+            for wait in wait_group {
+                let summary = wait.await;
+            }
+        }));
+    }
+
+    fn execute(&mut self, txn: Transaction) -> Result<(), anyhow::Error> {
+        // Engine should start before execute any transaction
+        if self.shutdown.is_none() {
+            panic!("Engine not started yet");
+        }
+
+        // simple dispatcher with hashing
+        let client_id = txn.client_id();
+        self.endpoint[txn.client_id() as usize % self.endpoint.len()].send(txn)?;
+
         Ok(())
+    }
+
+    async fn shutdown(mut self) {
+        if let Some(shutdown) = self.shutdown {
+            // close each service by dropping each corresponding endpoint
+            self.endpoint.into_iter().for_each(|ep| {
+                drop(ep);
+            });
+
+            // wait for all services to finish
+            shutdown.await;
+        }
     }
 }
 
@@ -147,6 +184,8 @@ struct PaymentService {
     ledger: Vec<Transaction>, // FIFO
 }
 
+type AssetBook = std::collections::HashMap<ClientID, Asset>;
+
 impl PaymentService {
     fn new() -> Self {
         Self {
@@ -154,183 +193,168 @@ impl PaymentService {
         }
     }
 
-    async fn start(
+    async fn run(
         mut self,
-        client: ClientID,
-        dead_letter_queue: tokio::sync::mpsc::Sender<Result<(), PaymentError>>,
-    ) -> Result<ServiceEndpoint, anyhow::Error> {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-        let async_work = async move {
-            let mut asset = Asset::new();
-            let mut is_locked = None;
+        mut receiver: mpsc::UnboundedReceiver<Transaction>,
+        dead_letter_queue: tokio::sync::mpsc::UnboundedSender<PaymentError>,
+    ) -> Result<PaymentSummary, anyhow::Error> {
+        let mut asset_book = AssetBook::new();
 
-            while let Some(txn) = rx.recv().await {
-                // The instruction seemed not mention what to do for the following transactions once it's locked
-                // Here I choose to simply skip such transaction and send an error back
-                if let Some(tx) = is_locked {
-                    dead_letter_queue
-                        .send(Err(PaymentError::Locked(txn, tx)))
-                        .await?;
-                    continue;
-                }
+        while let Some(txn) = receiver.recv().await {
+            // make new asset for new client
+            let mut asset = asset_book
+                .entry(txn.client_id())
+                .or_insert_with(|| Asset::new());
 
-                // insert new record to ledger
-                self.ledger.push(txn.clone());
-
-                let tx = txn.tx();
-                match (txn, self.running.get_mut(&tx)) {
-                    // Case 1. Deposit
-                    (Transaction::Deposit(client, tx, amount), None) => {
-                        let txn = TransactionInner {
-                            client,
-                            tx,
-                            amount,
-                            status: DisputeStatus::Normal,
-                        };
-
-                        self.running.insert(tx, txn);
-                        asset.total += amount;
-                        asset.available += amount;
-                    }
-                    // Case 2. Error for deposit if found duplicate tx
-                    (txn @ Transaction::Deposit(..), Some(_)) => {
-                        dead_letter_queue
-                            .send(Err(PaymentError::DuplicateTX(txn)))
-                            .await?;
-                    }
-
-                    // Case 3. Withdraw
-                    (Transaction::Withdraw(client, tx, amount), None) => {
-                        // check if insuffiecient balance
-                        if asset.available - amount < 0.0 {
-                            dead_letter_queue
-                                .send(Err(PaymentError::InsuffiecientBalance(
-                                    Transaction::Withdraw(client, tx, amount),
-                                    asset.available,
-                                )))
-                                .await;
-                            continue;
-                        }
-
-                        // happy path
-                        let txn = TransactionInner {
-                            client,
-                            tx,
-                            amount: -amount, // negate to indicate it's a withdraw move
-                            status: DisputeStatus::Normal,
-                        };
-
-                        self.running.insert(tx, txn);
-
-                        asset.total -= amount;
-                        asset.available -= amount;
-                    }
-
-                    // Case 4. Error for withdraw if found duplicated tx
-                    (txn @ Transaction::Withdraw(..), Some(_)) => {
-                        dead_letter_queue
-                            .send(Err(PaymentError::DuplicateTX(txn)))
-                            .await?;
-                    }
-
-                    // Case 5. Dispute from normal status
-                    (
-                        txn @ Transaction::Dispute { .. },
-                        Some(TransactionInner {
-                            amount,
-                            status: ref mut st @ DisputeStatus::Normal,
-                            ..
-                        }),
-                    ) => {
-                        if asset.available - *amount < 0.0 {
-                            dead_letter_queue
-                                .send(Err(PaymentError::InsuffiecientBalance(
-                                    txn,
-                                    asset.available,
-                                )))
-                                .await?;
-                            continue;
-                        }
-                        asset.available -= *amount;
-                        asset.hold += *amount;
-                        // update status because it's disputed
-                        *st = DisputeStatus::Disputed;
-                    }
-
-                    // Case 6. Resolve from disputed status
-                    (
-                        Transaction::Resolve { .. },
-                        Some(TransactionInner {
-                            amount,
-                            status: ref mut st @ DisputeStatus::Disputed,
-                            ..
-                        }),
-                    ) => {
-                        asset.available += *amount;
-                        asset.hold -= *amount;
-                        // update status because it's resolved and no loger disputed
-                        *st = DisputeStatus::Normal;
-                    }
-
-                    // Case 7. Chargeback from disputed status
-                    (
-                        Transaction::ChargeBack { .. },
-                        Some(TransactionInner {
-                            tx,
-                            amount,
-                            status: ref mut st @ DisputeStatus::Disputed,
-                            ..
-                        }),
-                    ) => {
-                        asset.total -= *amount;
-                        asset.hold -= *amount;
-                        // instruction said it's locked when chargeback
-                        is_locked = Some(*tx);
-
-                        // update status since it's charged back
-                        *st = DisputeStatus::ChargeBacked;
-                    }
-
-                    // Case 8. Error for Resolve | ChargeBack if any but disputed status
-                    (
-                        txn @ (Transaction::ChargeBack { .. } | Transaction::Resolve { .. }),
-                        Some(TransactionInner { status, .. }),
-                    ) => {
-                        dead_letter_queue
-                            .send(Err(PaymentError::InvalidDisputeStatus(txn, *status)))
-                            .await?;
-                    }
-
-                    // Case 8. Error for no such tx
-                    (txn, None) => {
-                        dead_letter_queue
-                            .send(Err(PaymentError::NoSuchTX(txn)))
-                            .await?;
-                    }
-
-                    // Case 9. Error for the remain combinations
-                    (txn @ _, txn_inner @ _) => {
-                        dead_letter_queue
-                            .send(Err(PaymentError::Unknown(txn, txn_inner.cloned())))
-                            .await?;
-                    }
-                }
+            // The instruction seemed not mention what to do for the following transactions once it's locked
+            // Here I choose to simply skip transaction and publish an error
+            if let Some(tx) = asset.is_locked {
+                let _ = dead_letter_queue.send(PaymentError::Locked(txn, tx));
+                continue;
             }
 
-            let finalize = FinanceBook {
-                client,
-                asset,
-                is_locked: is_locked.is_some(),
-                ledger: self.ledger,
-                //disputed_records: self.disputed_records,
-            };
+            // for tracking
+            self.ledger.push(txn.clone());
 
-            Ok::<FinanceBook, anyhow::Error>(finalize)
+            let tx = txn.tx();
+            match (txn, self.running.get_mut(&tx)) {
+                // Case 1. Deposit
+                (Transaction::Deposit(client, tx, amount), None) => {
+                    let txn = TransactionInner {
+                        client,
+                        tx,
+                        amount,
+                        status: DisputeStatus::Normal,
+                    };
+
+                    self.running.insert(tx, txn);
+
+                    asset.total += amount;
+                    asset.available += amount;
+                }
+                // Case 2. Error for deposit if found duplicate transaction id
+                (txn @ Transaction::Deposit(..), Some(_)) => {
+                    dead_letter_queue.send(PaymentError::DuplicateTransaction(txn));
+                }
+
+                // Case 3. Withdraw
+                (Transaction::Withdraw(client, tx, amount), None) => {
+                    // check if insuffiecient balance
+                    if asset.available - amount < 0.0 {
+                        dead_letter_queue.send(PaymentError::InsuffiecientBalance(
+                            Transaction::Withdraw(client, tx, amount),
+                            asset.available,
+                        ));
+                        continue;
+                    }
+
+                    // happy path
+                    let txn = TransactionInner {
+                        client,
+                        tx,
+                        amount: -amount, // negate to indicate it's a withdraw move
+                        status: DisputeStatus::Normal,
+                    };
+
+                    self.running.insert(tx, txn);
+
+                    asset.total -= amount;
+                    asset.available -= amount;
+                }
+
+                // Case 4. Error for withdraw if found duplicated tx
+                (txn @ Transaction::Withdraw(..), Some(_)) => {
+                    dead_letter_queue.send(PaymentError::DuplicateTransaction(txn));
+                }
+
+                // Case 5. Dispute from normal status
+                (
+                    txn @ Transaction::Dispute { .. },
+                    Some(TransactionInner {
+                        ref amount,
+                        status: ref mut st @ DisputeStatus::Normal,
+                        ..
+                    }),
+                ) => {
+                    // Error if inner is a withdraw transaction
+                    if amount < &0.0 {
+                        dead_letter_queue
+                            .send(PaymentError::InsuffiecientBalance(txn, asset.available));
+                        continue;
+                    }
+                    // Error if available fund is smaller than std::abs(amount)
+                    if asset.available - amount < 0.0 {
+                        dead_letter_queue
+                            .send(PaymentError::InsuffiecientBalance(txn, asset.available));
+                        continue;
+                    }
+                    // Happy path
+                    asset.available -= amount;
+                    asset.hold += amount;
+                    // update status because it's disputed
+                    *st = DisputeStatus::Disputed;
+                }
+
+                // Case 6. Resolve from disputed status
+                (
+                    Transaction::Resolve { .. },
+                    Some(TransactionInner {
+                        amount,
+                        status: ref mut st @ DisputeStatus::Disputed,
+                        ..
+                    }),
+                ) => {
+                    asset.available += *amount;
+                    asset.hold -= *amount;
+                    // update status because it's resolved and no loger disputed
+                    *st = DisputeStatus::Normal;
+                }
+
+                // Case 7. Chargeback from disputed status
+                (
+                    Transaction::ChargeBack { .. },
+                    Some(TransactionInner {
+                        tx,
+                        ref amount,
+                        status: ref mut st @ DisputeStatus::Disputed,
+                        ..
+                    }),
+                ) => {
+                    asset.total -= amount;
+                    asset.hold -= amount;
+                    // instruction said it's locked when chargeback
+                    asset.is_locked = Some(*tx);
+
+                    // update status since it's charged back
+                    *st = DisputeStatus::ChargeBacked;
+                }
+
+                // Case 8. Error for Resolve | ChargeBack if any but disputed status
+                (
+                    txn @ (Transaction::ChargeBack { .. } | Transaction::Resolve { .. }),
+                    Some(TransactionInner { status, .. }),
+                ) => {
+                    dead_letter_queue.send(PaymentError::InvalidDisputeStatus(txn, *status));
+                }
+
+                // Case 8. Error for no such tx
+                (txn, None) => {
+                    dead_letter_queue.send(PaymentError::NoSuchTransactionID(txn));
+                }
+
+                // Case 9. Error for the remain combinations
+                (txn @ _, txn_inner @ _) => {
+                    dead_letter_queue.send(PaymentError::Unknown(txn, txn_inner.cloned()));
+                }
+            }
+        }
+
+        let summary = PaymentSummary {
+            asset_book,
+            ledger: self.ledger,
         };
 
-        tokio::spawn(async_work);
-
-        Ok(tx)
+        Ok::<PaymentSummary, anyhow::Error>(summary)
     }
 }
 
